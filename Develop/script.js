ed0c510268a7d36501e55b34961c819d1b3a720f
John Magnuson <sobewon@gmail.com>
2023-01-19 11:27:06 -0500

    finished js code

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,47 @@
 // Assignment code here
+var charsL = "abcdefghijklmnopqrstuvwxyz"
+var charsU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = "0123456789"
+var charsS = "!@#$%^&*()"
+
+function generatePassword() {
+  var charsAll = ""
+  var pass = ""
+  var passLength = prompt("How many characters would you like your password to be? \n(Must be at least 8 and less than 128 characters)\nDefault is  set to 8");
+  var lower = confirm("Include lower case letters?")
+  var upper = confirm("Include UPPER CASE letters?")
+  var numb = confirm("Include numbers?")
+  var special = confirm("Include special characters? \nExample; !@#$%^&*()")
+
+  // if statement to set default pass to 8 even when no input is selected
+  if (passLength === "") {
+    passLength = 8
+  }
+  // if statements to combine all wanted characters into a single string
+  if (lower === true) {
+    charsAll = charsL
+    console.log("lowers")
+  }
+  if (upper === true) {
+    charsAll = charsAll + charsU
+    console.log("uppers")
+  }
+  if (numb === true) {
+    charsAll = charsAll + numbers
+    console.log("numbers")
+  }
+  if (special === true) {
+    charsAll = charsAll + charsS
+    console.log("spec")
+  }
+
+  for (var i=0; i < passLength; i++) {
+    pass = pass + charsAll[Math.floor(Math.random() * charsAll.length)]
+  }
+  console.log(pass)
+  return pass
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
